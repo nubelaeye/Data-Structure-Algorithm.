c80306d2a4319c12d86a0ba8abf8545388e7665d
King is Back <115486469+nubelaeye@users.noreply.github.com>
2022-11-18 05:29:30 +0530

    DSA - 2 Homework

---
+++ Lecture2_Homework.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Lecture2_Homework {
+    public static void main(String[] args) {
+     
+        // HomeWork number 1.
+
+        try (Scanner sc = new Scanner(System.in)) {
+        }
+
+        // System.out.println("Enter the first number: ");
+        // int a = sc.nextInt();
+
+        // System.out.println("Enter the second number: ");
+        // int b = sc.nextInt();
+
+        // System.out.println("Enter the third number: ");
+        // int c = sc.nextInt();
+
+        // float sum = (a+b+c);
+        // float avg = (sum/3);
+        // System.out.println("The average of the three numbers is: "+avg);
+
+        // HomeWork number 2.
+
+        // System.out.println("Enter the number: ");
+        // int a = sc.nextInt();
+        // int square = (a*a);
+        // System.out.println("The area of square is: "+square);
+
+        // HomeWork number 3.
+
+        // System.out.println("Enter the first item: ");
+        // float a = sc.nextFloat();
+        // System.out.println("Enter the second item: ");
+        // float b = sc.nextFloat();
+        // System.out.println("Enter the third item: ");
+        // float c = sc.nextFloat();
+        // float total = (a+b+c);
+        // System.out.println("The user total cost is: "+total);
+
+        // HomeWork number 4.
+
+        // answer is : 51,165.0848
+
+        // Homework number 5.
+
+        // yes it can be a variable;
+    }
+
+}

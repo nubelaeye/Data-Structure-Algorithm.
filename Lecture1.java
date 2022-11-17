9ecfd87851c9be87bb1842fcbbeb807b56bf9f83
King is Back <115486469+nubelaeye@users.noreply.github.com>
2022-11-18 05:23:02 +0530

    DSA - 1

---
+++ Lecture1.java
@@ -0,0 +1,23 @@
+public class Lecture1 {
+    public static void main(String[] args){
+        System.out.println("And after one era we have started to code now...\n");
+        // but at first we have to print * * * *
+        //                               * * *
+        //                               * *
+        //                               *
+        // So let's start now...
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("* \n");
+        System.out.println("Ok then, here we go now !");
+
+        // OK here we go for primitive data type..
+        byte i = 127;
+        System.out.println(i);
+        char c = 'z';
+        System.out.println(c);
+        boolean a = true;
+        System.out.println(a);
+    }
+}

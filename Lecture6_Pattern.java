8af16dd3573e153280543aae56e261894286b111
King is Back <115486469+nubelaeye@users.noreply.github.com>
2022-11-18 05:38:17 +0530

    DSA - 6

---
+++ Lecture6_Pattern.java
@@ -0,0 +1,86 @@
+import java.util.*;;
+public class Lecture6_Patterns{
+    public static void main(String[] args) {
+
+        try (Scanner sc = new Scanner(System.in)) {
+        
+        System.out.println("Enter the number: ");
+
+        // Pattern 1.
+        /*
+            * 
+            * *
+            * * * 
+            * * * *
+         */
+
+        // int a = sc.nextInt();
+        // System.out.println();
+        // for(int i = 1;i<=a;i++){
+        //     for(int j = 1;j<=i;j++){
+        //         System.out.print("*"+" ");
+        //     }
+        //     System.out.print("\n");
+        // }
+
+        
+        // System.out.print("\n");
+
+        // Pattern 2.
+         
+        /*
+            * * * *
+            * * *
+            * *
+            * 
+         */
+
+        // int n = sc.nextInt();
+        // System.out.println();
+        // for(int i = 1;i<=n;i++){
+        //     for(int j = 1;j<=n-i+1;j++){
+        //         System.out.print("*"+" ");
+        //     }
+        //     System.out.print("\n");
+        // }
+
+        // Pattern 3.
+
+            /*
+                1
+                1 2
+                1 2 3
+                1 2 3 4 
+             */
+
+            // int n = sc.nextInt();
+            // System.out.println();
+            // for(int i = 1;i<=n;i++){
+            //     for(int j = 1;j<=i;j++){
+            //         System.out.print(j);
+            //     }
+            //     System.out.print("\n");
+            // }
+
+            // Pattern 4.
+
+            /*
+             * A
+             * BC
+             * DEF
+             * GHIJ
+             */
+
+            char c = ' ';
+            int n = sc.nextInt();
+            System.out.println();
+            for(int i = 1;i<=n;i++){
+                for(int j = 1;j<=i;j++){
+                    System.out.print(c);
+                    c++;
+                }
+                System.out.print("\n");
+            }
+            
+    }
+}}

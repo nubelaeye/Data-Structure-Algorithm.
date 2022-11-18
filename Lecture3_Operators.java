7697ceaf0682f6b55c7446c29071f18855c94e8b
King is Back <115486469+nubelaeye@users.noreply.github.com>
2022-11-18 05:30:43 +0530

    DSA - 3

---
+++ Lecture3_Operators.java
@@ -0,0 +1,49 @@
+import java.util.*;;
+public class Lecture3_Operators {
+    public static void main(String[] args) {
+        try (Scanner c = new Scanner(System.in)) {
+        }
+        
+        // There are basically 5 types of increment..
+
+        // int a = 5;
+        // int b = 10;
+        // System.out.println(a+b);
+        // System.out.println(b-a);
+        // System.out.println(a*b);
+        // System.out.println(b/a);
+        // System.out.println(b%a+"\n");
+
+        // Unary operator has two types of increment..
+        // 1. Pre Increment. ++a;
+        // 2.Post Increment. a++;
+
+        // Using both the increments..
+        // System.out.println(a);
+        // System.out.println(++a);
+        // System.out.println(a);
+        // System.out.println(a++);
+        // System.out.println(a);
+        
+    // variable declaration
+    // int n1 = 5, n2 = 10, max;
+ 
+    // Largest among n1 and n2
+
+    // max = (n1 > n2) ? n1 : n2;
+ 
+    // Print the largest number.
+
+    // System.out.println("Largest number between "+ n1+ " and " + n2+ " is " + max); 
+
+    // int x = 10; int y = 5;
+
+    // int first = (y * (x / y + x / y));
+    // int exp2 = (y * x / y + y * x / y);
+
+    // System.out.println(first);
+    // System.out.println(exp2);
+
+    // int x = 9, y = 12;int a = 2, b = 4, c = 6;int exp = 4/3 * (x + 34) + 9 * (a + b * c) + (3 + y * (2 + a)) / (a + b*y);System.out.println(exp);
+    }    
+}

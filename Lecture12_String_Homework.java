5738ceb90e0d4b966a5a8eab57285978dd7864b7
King is Back <115486469+nubelaeye@users.noreply.github.com>
2022-12-11 09:58:55 +0530

    DSA - String Homework

---
+++ Lecture12_String_Homework.java
@@ -0,0 +1,24 @@
+public class Lecture12_String_Homework {
+    public static void main(String[] args) {
+
+        // Question number 1.
+        String name = "aie";
+        int count = 0;
+        for(int i = 0; i<name.length();i++){
+            char ch = name.charAt(i);
+            if(ch=='a'|| ch == 'i' || ch == 'o' || ch == 'u' || ch == 'e'){
+                count++;
+            }
+        }
+        System.out.println(count);
+
+        // Question number second : false and true...
+
+        // Question number third OUTPUT : ApnaCoege...
+
+        // Question number fifth : Functions about the string builder..
+        StringBuffer sb = new StringBuffer("Hello ");
+        sb.append("Java"); // now original string is changed
+        System.out.println(sb);
+    }
+}
